7c7ae05260090400cf94f93a373d8bcf3a3962b3
iofly <iofly@users.noreply.github.com>
2017-06-26 22:21:53 +0100

    Added option to specify output file name

--- ExcelToTable/Program.cs
+++ ExcelToTable/Program.cs
@@ -31,6 +31,7 @@ namespace ExcelToTable
             #region Define supported arguments
             List<SimpleArg> supportedArgs = new List<SimpleArg>();
             supportedArgs.Add(new SimpleArg { Name = "-filename", IsSwitch = false, Required = true, DefaultValue = null, ArgType = SimpleArgType.String, ExmaplePlaceholder="excelfilename", Description="Required. The Microsoft Excel file name" });
+            supportedArgs.Add(new SimpleArg { Name = "-outfile", IsSwitch = false, Required = false, DefaultValue = null, ArgType = SimpleArgType.String, ExmaplePlaceholder = "outputfilename", Description = "Optional. Output file. Defaults to [excelfilename] with format specific extension appended." });
             supportedArgs.Add(new SimpleArg { Name = "-format", IsSwitch = false, Required = false, DefaultValue = "html", ArgType = SimpleArgType.String, ExmaplePlaceholder = "html|wikitable|jsonsobjects|jsonarrays", Description = "Optional. Output file format [html|wikitable|jsonsobjects|jsonarrays]. Defaults to html." });
             supportedArgs.Add(new SimpleArg { Name = "-worksheet", IsSwitch = false, Required = false, DefaultValue = 1, ArgType = SimpleArgType.Integer, ExmaplePlaceholder = "1-n", Description = "Optional. A one-based index of the worksheet to export data from. Defaults to 1." });
             supportedArgs.Add(new SimpleArg { Name = "-range", IsSwitch = false, Required = false, DefaultValue = null, ArgType = SimpleArgType.String, ExmaplePlaceholder = "excelrange", Description = "Optional. Excel cell range to export. e.g. A12:C23. Defaults to the worksheet's used extents." });
@@ -59,6 +60,14 @@ namespace ExcelToTable
             {
                 ar["-filename"] = System.IO.Path.GetFullPath(ar["-filename"]);
             }
+
+
+            string outfile = string.Empty;
+            if(ar.ContainsKey("-outfile"))
+            {
+                outfile = System.IO.Path.GetFullPath(ar["-outfile"]);
+            }
+
 
 
             WorkSheetRangeCoordinates wsrc = null;
@@ -118,9 +127,15 @@ namespace ExcelToTable
                 fileext = ".html";
             }
 
-            
 
-            System.IO.File.WriteAllText(System.IO.Path.GetFileName(ar["-filename"]) + fileext, text);
+            if (!String.IsNullOrWhiteSpace(outfile))
+            {
+                System.IO.File.WriteAllText(outfile, text);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(System.IO.Path.GetFileName(ar["-filename"]) + fileext, text);
+            }
             #endregion
         }
     }
